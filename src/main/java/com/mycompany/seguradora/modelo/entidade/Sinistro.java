c19b9fae2c17d5da730085959acb9f098cfa89d3
Linicker <olinicker@gmail.com>
2024-08-09 21:41:08 -0300

    aplicação de estilos

---
+++ src/main/java/com/mycompany/seguradora/modelo/entidade/Sinistro.java
@@ -0,0 +1,14 @@
+package com.mycompany.seguradora.modelo.entidade;
+
+import java.util.Calendar;
+
+
+public class Sinistro {
+    
+    private Integer idSinistro;
+    private Calendar dataOcorrenciaSinistro;
+    private String descricaoSinistro;
+    private Double valorReparacaoSinistro;
+    private Seguro seguroSinistro;
+    
+}
